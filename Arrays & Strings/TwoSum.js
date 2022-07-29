6d03b556b6df7ae9c71d4ff76e3b8d51b9137ec6
Trend20 <enockomondi305@gmail.com>
2022-07-29 23:56:57 +0300

    two sum solution

--- Arrays & Strings/TwoSum.js
+++ Arrays & Strings/TwoSum.js
@@ -5,13 +5,16 @@ The function twoSum should return indices of the two numbers such that they add 
 the target, where index1 must be less than index2. Please note that your returned answers
 (both index1 and index2) are not zero-based.
 You may assume that each input would have exactly one solution.
+
+Related topic
+  ** Arrays
+  ** Hash Table
 */
 
 // Solution 1
 // Brute force O(n^2)
-const twoSum = (n) =>{
+const twoSum1 = (n, target) =>{
   n.sort();
-  let target = 9;
   for(let i = 0; i < n.length; i++){
     for(let j = i + 1; j < n.length; j++){
       if(n[i] + n[j] == target){
@@ -20,10 +23,22 @@ const twoSum = (n) =>{
     }
   }
 }
-
 let myArray = [1, 9, 3, 7, 5, 2, 7, 4, 8];
-twoSum(myArray);
+let myTarget = 9;
+twoSum(myArray, myTarget);
 
 
 // Solution 2
 // Hash Table
+
+const twoSum2 = (n, target) =>{
+  const keys = {};
+  // loop through the array n
+  for(let i = 0; i < n.length; i++){
+    let val = n[i];
+    if(keys[target - val] != null){
+      return [keys[target - val], i]
+    }
+    keys[val] = i;
+  }
+}
